1afe88ae6964f36c12730a1cbd7445d5edf354b9
Richard <isit_ric@hotmail.com>
2022-06-17 16:54:43 +0100

    commit12: unHash added

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -7,7 +7,6 @@ exports.hashPassword = async (req,res, next)=>{ //This will go on the create use
     try {
         // change value of password to a hash value
         req.body.password = await bcrypt.hash(req.body.password, 8); //convert to hash. hash takes two objects, the pass and 'salt'. the higyher the salt number, the more complex the result
-        console.log("hash password");
         next();// Dont forget to put your next funct in to ensure that the req,res continues
     } catch (error) {
         console.log(error);
@@ -18,7 +17,7 @@ exports.hashPassword = async (req,res, next)=>{ //This will go on the create use
 //decrypt hash password and compare
 exports.unHash = async (req,res,next)=>{
     try {
-        req.user = User.findOne({username: req.body.username});
+        req.user = await User.findOne({username: req.body.username});
         const result = await bcrypt.compare(req.body.password, req.user.password);
         if (result){
             next();
--- src/server.js
+++ src/server.js
@@ -5,7 +5,6 @@ const port = process.env.PORT || 5001;//check for correct port or use 5001
 const userRouter = require("./user/routes");
 
 app.use(express.json());
-
 app.use(userRouter);//Note the order of commands
 
 app.listen(port, ()=>{//By placing port number in variable, we can change the port number and the instruction stays the same
